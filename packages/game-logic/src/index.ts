2fb1bb0743a9d2d03955337ea9ec066c18fc5425
nexusaileads-dot <nexusaileads@gmail.com>
2026-04-13 22:47:47 +0100

    Create index.ts

---
+++ packages/game-logic/src/index.ts
@@ -0,0 +1,7 @@
+export * from './entities/Player';
+export * from './entities/Enemy';
+export * from './entities/Boss';
+export * from './entities/Projectile';
+export * from './systems/CombatSystem';
+export * from './systems/WaveSystem';
+export * from './systems/RewardCalculator';
